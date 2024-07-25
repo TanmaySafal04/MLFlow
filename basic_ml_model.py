253749648e7df60ec592223c36fef4ab0935d51c
TanmaySafal04 <tanmaysafal.4@gmail.com>
2024-07-26 01:54:23 +0530

    New code added

--- basic_ml_model.py
+++ basic_ml_model.py
@@ -8,7 +8,7 @@ import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import ElasticNet
 
-from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,accuracy_score
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,accuracy_score,roc_auc_score
 from sklearn.model_selection import train_test_split
 
 from ucimlrepo import fetch_ucirepo 
@@ -34,43 +34,35 @@ def get_data():
     except Exception as e:
         raise e
 
+with mlflow.start_run():
+    def main(n_estimators,max_depth):
+        df = get_data()
+        #Train test split
+        train,test=train_test_split(df)
+        x_train = train.drop(['quality'],axis=1)
+        x_test = test.drop(['quality'],axis=1)
 
-def main(n_estimators,max_depth):
-    df = get_data()
-    #Train test split
-    train,test=train_test_split(df)
-    x_train = train.drop(['quality'],axis=1)
-    x_test = test.drop(['quality'],axis=1)
+        y_train = train['quality']
+        y_test = test['quality']
+       
+        rf=RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth)
+        rf.fit(x_train,y_train)
+        pred = rf.predict(x_test)
+        prob_pred = rf.predict_proba(x_test)
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("n_estimators", max_depth)
+        return pred,y_test,prob_pred
 
-    y_train = train['quality']
-    y_test = test['quality']
-    # Model training
-    # lr = ElasticNet()
-    # lr.fit(x_train,y_train)
-    # pred = lr.predict(x_test)
-    rf=RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth)
-    rf.fit(x_train,y_train)
-    pred = rf.predict(x_test)
-    return pred,y_test
-
-def evaluate(y_pred,y_true):
-    #y_pred,y_true = main()
-    # mae = mean_absolute_error(y_true,y_pred)
-    # mse = mean_squared_error(y_true,y_pred)
-    # rmse = np.sqrt(mse)
-    # r2 = r2_score(y_true,y_pred)
-    accuracy = accuracy_score(y_true,y_pred)
-    print(f"Accuracy Score: {accuracy}")
-    return accuracy
-    #return mae,mse,rmse,r2
-
-# #mae,mse,rmse,r2=evaluate()
-# accuracy = evaluate()
-# # print(f"Mean Absolute Error: {mae}")
-# # print(f"Mean Squared Error: {mse}")
-# # print(f"Root Mean Squared Error: {rmse}")
-# # print(f"R2 Score: {r2}")
-# print(f"Accuracy Score: {accuracy}")
+    def evaluate(y_pred,y_true,prob_pred):
+        
+        accuracy = accuracy_score(y_true,y_pred)
+        roc = roc_auc_score(y_true,prob_pred,multi_class='ovr')
+        mlflow.log_param("Accuracy", accuracy)
+        mlflow.log_param("ROC_AUC_score Score", roc)
+        print(f"Accuracy Score: {accuracy}")
+        print(f"ROC_AUC_score Score: {roc}")
+        return accuracy,roc
+        #return mae,mse,rmse,r2
 
 
 
@@ -81,8 +73,8 @@ if __name__ == '__main__':
     args.add_argument("--max_depth","-m", default=5, type = int)
     parse_args=args.parse_args()
     try:
-        y_pred,y_true = main(n_estimators = parse_args.n_estimators, max_depth = parse_args.max_depth)
-        evaluate(y_pred,y_true)    
+        y_pred,y_true,pred_prob = main(n_estimators = parse_args.n_estimators, max_depth = parse_args.max_depth)
+        evaluate(y_pred,y_true,pred_prob)    
     except Exception as e:
         raise e
 
